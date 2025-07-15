9aa910eec86e4826cb59db1a9a6399ee77e431cb
Mikey-kun10 <azeezoluwadamilare82@gmail.com>
2025-07-15 11:14:58 +0100

    Recuiter urls

--- backend/recruiters/urls.py
+++ backend/recruiters/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-from .views import RecruiterProfileView, RecruiterListView
-
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .auth_views import RecruiterProfileView, RecruiterListView
+from .views import JobPostViewSet, EmployerAnalyticsViewSet
 urlpatterns = [
     path('profile/', RecruiterProfileView.as_view(), name='recruiter-profile'),
     path('', RecruiterListView.as_view(), name='recruiter-list'),
+]
+router = DefaultRouter()
+router.register('jobs', JobPostViewSet, basename='jobpost')
+router.register('analytics', EmployerAnalyticsViewSet, basename='employer-analytics')
+
+urlpatterns = [
+    path('', include(router.urls)),
 ]
