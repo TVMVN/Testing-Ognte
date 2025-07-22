531fbbf149489f141972b57a3f84f55f0ff59e90
Eli <laseolufemi47@gmail.com>
2025-07-22 09:23:23 +0100

    migration error handled

--- backend/applications/migrations/0008_rename_skills_jobpost_required_skills_and_more.py
+++ backend/applications/migrations/0008_rename_skills_jobpost_required_skills_and_more.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='application',
             name='created_at',
-            field=models.DateTimeField(auto_now_add=True, default=10),
+            field=models.DateTimeField(auto_now_add=True),
             preserve_default=False,
         ),
         migrations.AddField(
